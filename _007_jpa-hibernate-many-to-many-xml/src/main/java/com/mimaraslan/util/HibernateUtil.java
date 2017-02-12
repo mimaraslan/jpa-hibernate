d0caedfcf9fa2fbb22dcf5adde4b5f77a643bc9c
mimaraslan <mimaraslan@gmail.com>
2017-02-12 13:08:07 +0300

    JPA/Hibernate
    JPA/Hibernate

---
+++ _007_jpa-hibernate-many-to-many-xml/src/main/java/com/mimaraslan/util/HibernateUtil.java
@@ -0,0 +1,33 @@
+package com.mimaraslan.util;
+
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.service.ServiceRegistry;
+
+public class HibernateUtil {
+
+	private static SessionFactory sessionFactory;
+	private static ServiceRegistry serviceRegistry;
+
+	static {
+		try {
+			Configuration configuration = new Configuration();
+			configuration.configure();
+			serviceRegistry = new StandardServiceRegistryBuilder()
+					.applySettings(configuration.getProperties()).build();
+			sessionFactory = configuration.buildSessionFactory(serviceRegistry);
+		} catch (Throwable ex) {
+			System.err.println("Error creating SessionFactory :" + ex);
+			throw new ExceptionInInitializerError(ex);
+		}
+	}
+
+	public static SessionFactory getSessionFactory() {
+		return sessionFactory;
+	}
+
+	public static void shutdown() {
+		sessionFactory.close();
+	}
+}
