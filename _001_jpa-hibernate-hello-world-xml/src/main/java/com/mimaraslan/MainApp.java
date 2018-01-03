0f04012974ca5fb018adb1d369fe573b39fefeea
mimaraslan <mimaraslan@gmail.com>
2018-01-03 19:30:40 +0300

    JPA/Hibernate
    JPA/Hibernate

--- _001_jpa-hibernate-hello-world-xml/src/main/java/com/mimaraslan/MainApp.java
+++ _001_jpa-hibernate-hello-world-xml/src/main/java/com/mimaraslan/MainApp.java
@@ -1,7 +1,10 @@
 package com.mimaraslan;
 
 import java.util.Date;
+
+import org.hibernate.HibernateException;
 import org.hibernate.Session;
+import org.hibernate.Transaction;
 
 import com.mimaraslan.model.Customer;
 import com.mimaraslan.util.HibernateUtil;
@@ -9,19 +12,27 @@ import com.mimaraslan.util.HibernateUtil;
 public class MainApp {
 
 	public static void main(String[] args) {
+
+		Customer customer = new Customer();
+
+		customer.setName("Mimar");
+		customer.setAddress("Türkiye İstanbul");
+		customer.setCreationDate(new Date());
+
 		Session session = HibernateUtil.getSessionFactory().openSession();
+		Transaction tx = null;
 		try {
-			session.beginTransaction();
-			Customer customer = new Customer();
-			customer.setName("Mimar");
-			customer.setAddress("Türkiye İstanbul");
-			customer.setCreationDate(new Date());
+			tx = session.beginTransaction();
 			session.save(customer);
-			session.getTransaction().commit();
-		} catch (Exception e) {
+			tx.commit();
+		} catch (HibernateException e) {
+			if (tx != null) {
+				tx.rollback();
+			}
 			System.err.println("Error creating Customer :" + e);
-			session.getTransaction().rollback();
+			e.printStackTrace();
+		} finally {
+			session.close();
 		}
-		HibernateUtil.shutdown();
 	}
 }
