9e05b87282ce2627205aaf78516ac1acf8519c7e
mimaraslan <mimaraslan@gmail.com>
2019-02-04 20:28:51 +0300

    JPA/Hibernate
    JPA/Hibernate

---
+++ _007_jpa-hibernate-many-to-many-xml/src/main/java/com/mimaraslan/model/Customer.java
@@ -0,0 +1,53 @@
+package com.mimaraslan.model;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class Customer {
+
+	private int customerId;
+	private String title;
+	private String name;
+	private Set<Address> addresses = new HashSet<Address>(0);
+
+	public Customer() {
+	}
+
+	public Customer(String title, String name, Set<Address> addresses) {
+		this.title = title;
+		this.name = name;
+		this.addresses = addresses;
+	}
+
+	public int getCustomerId() {
+		return customerId;
+	}
+
+	public void setCustomerId(int customerId) {
+		this.customerId = customerId;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public Set<Address> getAddresses() {
+		return addresses;
+	}
+
+	public void setAddresses(Set<Address> addresses) {
+		this.addresses = addresses;
+	}
+}
