6ccbcaa5e0dd610e8d888f292da9c76577392a99
mimaraslan <mimaraslan@gmail.com>
2019-09-12 23:40:06 +0300

    Hibernate Hello World with XML
    Hibernate Hello World with XML

---
+++ _01_Hibernate-Hello-World-XML/src/main/java/com/mimaraslan/persistence/Customer.java
@@ -0,0 +1,54 @@
+package com.mimaraslan.persistence;
+
+import java.util.Date;
+
+public class Customer {
+
+	private int id;
+	private String name;
+	private String address;
+	private Date creationDate;
+
+	public Customer() {
+	}
+
+	public Customer(int id, String name, String address, Date creationDate) {
+		this.id = id;
+		this.name = name;
+		this.address = address;
+		this.creationDate = creationDate;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public Date getCreationDate() {
+		return creationDate;
+	}
+
+	public void setCreationDate(Date creationDate) {
+		this.creationDate = creationDate;
+	}
+
+}
---
+++ _01_Hibernate-Hello-World-XML/src/test/java/com/mimaraslan/test/TestApp.java
@@ -0,0 +1,29 @@
+package com.mimaraslan.test;
+
+import java.util.Date;
+
+import org.hibernate.Session;
+
+import com.mimaraslan.persistence.Customer;
+import com.mimaraslan.util.HibernateUtil;
+
+public class TestApp {
+
+	public static void main(String[] args) {
+		Session session = HibernateUtil.getSessionFactory().openSession();
+		try {
+			session.beginTransaction();
+			Customer customer = new Customer();
+			customer.setName("Mimar");
+			customer.setAddress("Türkiye İstanbul");
+			customer.setCreationDate(new Date());
+			session.save(customer);
+			session.getTransaction().commit();
+		} catch (Exception e) {
+			System.err.println("Error creating Customer :" + e);
+			session.getTransaction().rollback();
+		}
+		HibernateUtil.shutdown();
+	}
+
+}
