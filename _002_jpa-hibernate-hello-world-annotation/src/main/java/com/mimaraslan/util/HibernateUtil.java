0edf0c94fda87ff1993c87cb981e34335313d39b
mimaraslan <mimaraslan@gmail.com>
2023-03-14 01:13:38 +0300

    JPA Hibernate ORM
    JPA Hibernate ORM

---
+++ _002_jpa-hibernate-hello-world-annotation/src/main/java/com/mimaraslan/util/HibernateUtil.java
@@ -0,0 +1,27 @@
+package com.mimaraslan.util;
+
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.service.ServiceRegistry;
+
+public class HibernateUtil {
+
+	private static ServiceRegistry serviceRegistry;
+
+	static {
+		try {
+			Configuration configuration = new Configuration();
+			configuration.configure();
+			serviceRegistry = new StandardServiceRegistryBuilder().applySettings(configuration.getProperties()).build();
+			configuration.buildSessionFactory(serviceRegistry);
+		} catch (Exception ex) {
+			System.err.println("Error creating SessionFactory :" + ex);
+			throw new ExceptionInInitializerError(ex);
+		}
+	}
+
+	public static SessionFactory getSessionFactory() {
+		return new Configuration().configure().buildSessionFactory();
+	}
+}
