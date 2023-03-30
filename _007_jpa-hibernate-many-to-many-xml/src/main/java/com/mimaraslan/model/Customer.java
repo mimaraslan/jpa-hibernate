518ce5860a6efecdc34c7f19b585c83d538bd208
mimaraslan <mimaraslan@gmail.com>
2023-03-31 01:15:00 +0300

    JPA Hibernate ORM
    JPA Hibernate ORM

--- _007_jpa-hibernate-many-to-many-xml/src/main/java/com/mimaraslan/model/Customer.java
+++ _007_jpa-hibernate-many-to-many-xml/src/main/java/com/mimaraslan/model/Customer.java
@@ -5,49 +5,49 @@ import java.util.Set;
 
 public class Customer {
 
-	private int customerId;
-	private String title;
-	private String name;
-	private Set<Address> addresses = new HashSet<Address>(0);
+    private int customerId;
+    private String title;
+    private String name;
+    private Set<Address> addresses = new HashSet<Address>(0);
 
-	public Customer() {
-	}
+    public Customer() {
+    }
 
-	public Customer(String title, String name, Set<Address> addresses) {
-		this.title = title;
-		this.name = name;
-		this.addresses = addresses;
-	}
+    public Customer(String title, String name, Set<Address> addresses) {
+        this.title = title;
+        this.name = name;
+        this.addresses = addresses;
+    }
 
-	public int getCustomerId() {
-		return customerId;
-	}
+    public int getCustomerId() {
+        return customerId;
+    }
 
-	public void setCustomerId(int customerId) {
-		this.customerId = customerId;
-	}
+    public void setCustomerId(int customerId) {
+        this.customerId = customerId;
+    }
 
-	public String getName() {
-		return name;
-	}
+    public String getName() {
+        return name;
+    }
 
-	public void setName(String name) {
-		this.name = name;
-	}
+    public void setName(String name) {
+        this.name = name;
+    }
 
-	public String getTitle() {
-		return title;
-	}
+    public String getTitle() {
+        return title;
+    }
 
-	public void setTitle(String title) {
-		this.title = title;
-	}
+    public void setTitle(String title) {
+        this.title = title;
+    }
 
-	public Set<Address> getAddresses() {
-		return addresses;
-	}
+    public Set<Address> getAddresses() {
+        return addresses;
+    }
 
-	public void setAddresses(Set<Address> addresses) {
-		this.addresses = addresses;
-	}
+    public void setAddresses(Set<Address> addresses) {
+        this.addresses = addresses;
+    }
 }
