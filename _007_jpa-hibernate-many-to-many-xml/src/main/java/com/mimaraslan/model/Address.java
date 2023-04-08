75191fbf54b62b7794b26bdf64b6672a5e5060d1
mimaraslan <mimaraslan@gmail.com>
2023-04-09 01:43:52 +0300

    JPA Hibernate ORM
    JPA Hibernate ORM

--- _007_jpa-hibernate-many-to-many-xml/src/main/java/com/mimaraslan/model/Address.java
+++ _007_jpa-hibernate-many-to-many-xml/src/main/java/com/mimaraslan/model/Address.java
@@ -5,68 +5,68 @@ import java.util.Set;
 
 public class Address {
 
-	private int addressId;
-	private String street;
-	private String city;
-	private String country;
-	private String type;
-	private Set<Customer> customers = new HashSet<Customer>(0);
+    private int addressId;
+    private String street;
+    private String city;
+    private String country;
+    private String type;
+    private Set<Customer> customers = new HashSet<Customer>(0);
 
-	public Address() {
-	}
+    public Address() {
+    }
 
-	public Address(String street, String city, String country, String type) {
-		this.street = street;
-		this.city = city;
-		this.country = country;
-		this.type = type;
-	}
+    public Address(String street, String city, String country, String type) {
+        this.street = street;
+        this.city = city;
+        this.country = country;
+        this.type = type;
+    }
 
-	public int getAddressId() {
-		return addressId;
-	}
+    public int getAddressId() {
+        return addressId;
+    }
 
-	public void setAddressId(int addressId) {
-		this.addressId = addressId;
-	}
+    public void setAddressId(int addressId) {
+        this.addressId = addressId;
+    }
 
-	public String getStreet() {
-		return street;
-	}
+    public String getStreet() {
+        return street;
+    }
 
-	public void setStreet(String street) {
-		this.street = street;
-	}
+    public void setStreet(String street) {
+        this.street = street;
+    }
 
-	public String getCity() {
-		return city;
-	}
+    public String getCity() {
+        return city;
+    }
 
-	public void setCity(String city) {
-		this.city = city;
-	}
+    public void setCity(String city) {
+        this.city = city;
+    }
 
-	public String getCountry() {
-		return country;
-	}
+    public String getCountry() {
+        return country;
+    }
 
-	public void setCountry(String country) {
-		this.country = country;
-	}
+    public void setCountry(String country) {
+        this.country = country;
+    }
 
-	public String getType() {
-		return type;
-	}
+    public String getType() {
+        return type;
+    }
 
-	public void setType(String type) {
-		this.type = type;
-	}
+    public void setType(String type) {
+        this.type = type;
+    }
 
-	public Set<Customer> getCustomers() {
-		return customers;
-	}
+    public Set<Customer> getCustomers() {
+        return customers;
+    }
 
-	public void setCustomers(Set<Customer> customers) {
-		this.customers = customers;
-	}
+    public void setCustomers(Set<Customer> customers) {
+        this.customers = customers;
+    }
 }
