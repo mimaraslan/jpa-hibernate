34407b9f6751a5241c7c8e45c14851c82e3d9cd8
mimaraslan <mimaraslan@gmail.com>
2023-04-30 01:45:28 +0300

    JPA Hibernate ORM
    JPA Hibernate ORM

--- _010_jpa-hibernate-crud-hql-xml/src/main/java/com/mimaraslan/util/HibernateUtil.java
+++ _010_jpa-hibernate-crud-hql-xml/src/main/java/com/mimaraslan/util/HibernateUtil.java
@@ -8,28 +8,30 @@ import org.hibernate.service.ServiceRegistry;
 
 public class HibernateUtil {
 
-	static Logger log = Logger.getLogger(HibernateUtil.class.getName());
-	
-	private static SessionFactory sessionFactory;
-	private static ServiceRegistry serviceRegistry;
+    static Logger log = Logger.getLogger(HibernateUtil.class.getName());
 
-	static {
-		try {
-			Configuration configuration = new Configuration();
-			configuration.configure();
-			serviceRegistry = new StandardServiceRegistryBuilder().applySettings(configuration.getProperties()).build();
-			sessionFactory = configuration.buildSessionFactory(serviceRegistry);
-		} catch (Throwable ex) {
-			log.error("Error creating SessionFactory :", ex);
-			throw new ExceptionInInitializerError(ex);
-		}
-	}
+    private static SessionFactory sessionFactory;
+    private static ServiceRegistry serviceRegistry;
 
-	public static SessionFactory getSessionFactory() {
-		return sessionFactory;
-	}
+    static {
+        try {
+            Configuration configuration = new Configuration();
+            configuration.configure();
+            serviceRegistry = new StandardServiceRegistryBuilder()
+                    .applySettings(configuration.getProperties())
+                    .build();
+            sessionFactory = configuration.buildSessionFactory(serviceRegistry);
+        } catch (Exception ex) {
+            log.error("Error creating SessionFactory :", ex);
+            throw new ExceptionInInitializerError(ex);
+        }
+    }
 
-	public static void shutdown() {
-		sessionFactory.close();
-	}
+    public static SessionFactory getSessionFactory() {
+        return sessionFactory;
+    }
+
+    public static void shutdown() {
+        sessionFactory.close();
+    }
 }
