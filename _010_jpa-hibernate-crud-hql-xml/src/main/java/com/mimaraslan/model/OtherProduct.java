d338b61ba58c17f8eaf2a08d2e349c1dc8ec0950
mimaraslan <mimaraslan@gmail.com>
2023-05-04 01:17:39 +0300

    JPA Hibernate ORM
    JPA Hibernate ORM

--- _010_jpa-hibernate-crud-hql-xml/src/main/java/com/mimaraslan/model/OtherProduct.java
+++ _010_jpa-hibernate-crud-hql-xml/src/main/java/com/mimaraslan/model/OtherProduct.java
@@ -4,70 +4,70 @@ import java.util.Date;
 
 public class OtherProduct {
 
-	private int productId;
-	private String productName;
-	private int quantity;
-	private Float unitPrice;
-	private Boolean discontinued;
-	private Date modifiedDate;
+    private int productId;
+    private String productName;
+    private int quantity;
+    private Float unitPrice;
+    private Boolean discontinued;
+    private Date modifiedDate;
 
-	public OtherProduct() {
-	}
+    public OtherProduct() {
+    }
 
-	public OtherProduct(String productName, int quantity, Float unitPrice,
-			Boolean discontinued, Date modifiedDate) {
-		this.productName = productName;
-		this.quantity = quantity;
-		this.unitPrice = unitPrice;
-		this.discontinued = discontinued;
-		this.modifiedDate = modifiedDate;
-	}
+    public OtherProduct(String productName, int quantity, Float unitPrice,
+                        Boolean discontinued, Date modifiedDate) {
+        this.productName = productName;
+        this.quantity = quantity;
+        this.unitPrice = unitPrice;
+        this.discontinued = discontinued;
+        this.modifiedDate = modifiedDate;
+    }
 
-	public int getProductId() {
-		return productId;
-	}
+    public int getProductId() {
+        return productId;
+    }
 
-	public void setProductId(int productId) {
-		this.productId = productId;
-	}
+    public void setProductId(int productId) {
+        this.productId = productId;
+    }
 
-	public String getProductName() {
-		return productName;
-	}
+    public String getProductName() {
+        return productName;
+    }
 
-	public void setProductName(String productName) {
-		this.productName = productName;
-	}
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
 
-	public int getQuantity() {
-		return quantity;
-	}
+    public int getQuantity() {
+        return quantity;
+    }
 
-	public void setQuantity(int quantity) {
-		this.quantity = quantity;
-	}
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
 
-	public Float getUnitPrice() {
-		return unitPrice;
-	}
+    public Float getUnitPrice() {
+        return unitPrice;
+    }
 
-	public void setUnitPrice(Float unitPrice) {
-		this.unitPrice = unitPrice;
-	}
+    public void setUnitPrice(Float unitPrice) {
+        this.unitPrice = unitPrice;
+    }
 
-	public Boolean getDiscontinued() {
-		return discontinued;
-	}
+    public Boolean getDiscontinued() {
+        return discontinued;
+    }
 
-	public void setDiscontinued(Boolean discontinued) {
-		this.discontinued = discontinued;
-	}
+    public void setDiscontinued(Boolean discontinued) {
+        this.discontinued = discontinued;
+    }
 
-	public Date getModifiedDate() {
-		return modifiedDate;
-	}
+    public Date getModifiedDate() {
+        return modifiedDate;
+    }
 
-	public void setModifiedDate(Date modifiedDate) {
-		this.modifiedDate = modifiedDate;
-	}
+    public void setModifiedDate(Date modifiedDate) {
+        this.modifiedDate = modifiedDate;
+    }
 }
