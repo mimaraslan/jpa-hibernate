e44dd6993f44d2882eee1fa69e36388797f1ad57
mimaraslan <mimaraslan@gmail.com>
2023-05-04 02:37:30 +0300

    JPA Hibernate ORM
    JPA Hibernate ORM

--- _003_jpa-hibernate6-hello-world-java-config/src/main/java/com/mimaraslan/AppMain.java
+++ _003_jpa-hibernate6-hello-world-java-config/src/main/java/com/mimaraslan/AppMain.java
@@ -5,13 +5,21 @@ import com.mimaraslan.entity.Student;
 
 import java.util.List;
 
-public class App {
+public class AppMain {
     public static void main(String[] args) {
+
         StudentDao studentDao = new StudentDao();
-        Student student = new Student("Ramesh", "Fadatare", "rameshfadatare@javaguides.com");
-        studentDao.saveStudent(student);
+
+        Student student1 = new Student("A", "X", "aaa@gmail.com");
+        Student student2 = new Student("B", "Y", "bbb@gmail.com");
+        Student student3 = new Student("C", "Z", "ccc@gmail.com");
+
+        studentDao.saveStudent(student1);
+        studentDao.saveStudent(student2);
+        studentDao.saveStudent(student3);
 
         List<Student> students = studentDao.getStudents();
-        students.forEach(s -> System.out.println(s.getFirstName()));
+        students.forEach(s -> System.out.println(s.getFirstName() + " " + s.getLastName() + " " + s.getEmail()));
+
     }
 }
--- _003_jpa-hibernate6-hello-world-java-config/src/main/java/com/mimaraslan/dao/StudentDao.java
+++ _003_jpa-hibernate6-hello-world-java-config/src/main/java/com/mimaraslan/dao/StudentDao.java
@@ -9,19 +9,19 @@ import java.util.List;
 public class StudentDao {
 	public void saveStudent(Student student) {
 		Transaction transaction = null;
+
 		try (Session session = HibernateUtil.getSessionFactory().openSession()) {
-			// start a transaction
 			transaction = session.beginTransaction();
-			// save the student object
 			session.save(student);
-			// commit transaction
 			transaction.commit();
 		} catch (Exception e) {
+
 			if (transaction != null) {
 				transaction.rollback();
 			}
 			e.printStackTrace();
 		}
+
 	}
 
 	public List<Student> getStudents() {
